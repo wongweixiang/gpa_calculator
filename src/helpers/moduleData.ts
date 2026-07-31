31f95e4daa91fb8b3364509cb16fc6441e6ec99f
WWX <wongweixiangjulian@mail.com>
2026-07-31 17:56:27 +0800

    feat: update modules

--- src/helpers/moduleData.ts
+++ src/helpers/moduleData.ts
@@ -1,53 +1,57 @@
 export const moduleData = [
   {
-    id: "IT1x11",
+    id: "IT1111",
     name: "Applied Mathematics in Computing",
     credits: 2,
   },
   {
-    id: "IT1x12",
+    id: "IT1112",
     name: "Business Innovation & Enterprise",
     credits: 4,
   },
   {
-    id: "IT1x13",
+    id: "IT1113",
     name: "Network Technologies",
     credits: 4,
   },
   {
-    id: "IT1x14",
+    id: "IT1114",
     name: "Programming",
     credits: 4,
   },
   {
-    id: "IT1x15",
+    id: "IT1115",
     name: "UX Design in Web Development",
     credits: 4,
   },
-    {
-    id: "IT1x21",
+  {
+    id: "IT1121",
     name: "AI & Data Analytics",
     credits: 4,
   },
   {
-    id: "IT1x22",
+    id: "IT1122",
     name: "Cybersecurity Technologies & Ethics",
     credits: 4,
   },
   {
-    id: "IT1x23",
+    id: "IT1123",
     name: "Database Design & Administration",
     credits: 4,
   },
   {
-    id: "IT1x24",
+    id: "IT1124",
     name: "Statistical Research Methods",
     credits: 4,
   },
   {
-    id: "IT1x25",
+    id: "IT1125",
     name: "Web Development Project",
     credits: 4,
   },
-  
+  { id: "IT2111", name: "Data Structures & Algorithms", credits: 4 },
+  { id: "IT2112", name: "Agile Development Process with Devops", credits: 4 },
+  { id: "IT2113", name: "Cloud Computing", credits: 4 },
+  { id: "IT2114", name: "Digital User Experience Design", credits: 4 },
+  { id: "IT2115", name: "Full Stack Application Development", credits: 4 },
 ];
